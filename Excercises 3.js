804db53b4f6009f89f4d0569386b080a69f4baab
mariorizki <mariorizki07@gmail.com>
2018-04-23 11:47:48 +0700

    perbaiki excercises 3

--- Excercises 3.js
+++ Excercises 3.js
@@ -1,19 +1,41 @@
-var nama = 'Budi';
-var peran = 'Archer'; //peran : Tank, Support, Archer
+var nama = 'Joko';  //nama : Budi, Joko, Ali
+var peran = 'Tank'; //peran : Tank, Support, Archer
 
+// Memilih nama Budi
 if (nama === 'Budi' && peran === 'Tank') {
-	console.log(
-		'"Selamat datang di Dunia Proxytia, Budi"\n"Halo, Tank Budi, kamu depat melindungi temanmu di bagian depan"');
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat melindungi temanmu di barisan depan"');
 } else if (nama === 'Budi' && peran === 'Support') {
-	console.log(
-		'"Selamat datang di Dunia Proxytia, Budi"\n"Halo, Support Budi, kamu depat menyembuhkan temanmu yang terluka"');
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menyembuhkan temanmu yang terluka"');
 } else if (nama === 'Budi' && peran === 'Archer') {
-	console.log(
-		'"Selamat datang di Dunia Proxytia, Budi"\n"Halo, Archer Budi, kamu dapat menghabisi musuh dari jarak jauh"');
-} else if (nama === '') {
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menghabisi musuh dari jarak jauh"');
+} 
+
+// Memilih nama Joko
+else if (nama === 'Joko' && peran === 'Tank'){
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat melindungi temanmu di barisan depan"');
+} else if (nama === 'Joko' && peran === 'Support') {
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menyembuhkan temanmu yang terluka"');
+} else if (nama === 'Joko' && peran === 'Archer') {
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menyembuhkan temanmu yang terluka"');
+} 
+
+// Memilih nama Ali
+else if (nama === 'Ali' && peran === 'Tank'){
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat melindungi temanmu di barisan depan"');
+} else if (nama === 'Ali' && peran === 'Support') {
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menyembuhkan temanmu yang terluka"');
+} else if (nama === 'Ali' && peran === 'Archer') {
+	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menyembuhkan temanmu yang terluka"');
+} 
+
+// Nama kosong
+else if (nama === '') {
 	console.log('"Nama harus diisi!"');
-} else if (nama === 'Budi') {
-	console.log('"Halo Budi, Pilih peranmu untuk memulai game!"');
+} 
+
+//Peran kosong
+else if (peran === '') {
+	console.log('"Halo ' + nama + ', Pilih peranmu untuk memulai game!"');
 } else {
 	console.log('"invalid input"');
 }
