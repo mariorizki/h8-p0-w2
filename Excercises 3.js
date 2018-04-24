8b326381ebad8f9979aa45fab4be0144cd1aeb95
mariorizki <mariorizki07@gmail.com>
2018-04-24 07:53:11 +0700

    minor revision

--- Excercises 3.js
+++ Excercises 3.js
@@ -1,31 +1,13 @@
-var nama = 'Joko';  //nama : Budi, Joko, Ali
-var peran = 'Tank'; //peran : Tank, Support, Archer
+var nama = '';  
+var peran = ''; //peran : Tank, Support, Archer
 
-// Memilih nama Budi
-if (nama === 'Budi' && peran === 'Tank') {
+
+if (nama !== '' && peran === 'Tank') {
 	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat melindungi temanmu di barisan depan"');
-} else if (nama === 'Budi' && peran === 'Support') {
+} else if (nama !== '' && peran === 'Support') {
 	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menyembuhkan temanmu yang terluka"');
-} else if (nama === 'Budi' && peran === 'Archer') {
+} else if (nama !== '' && peran === 'Archer') {
 	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menghabisi musuh dari jarak jauh"');
-} 
-
-// Memilih nama Joko
-else if (nama === 'Joko' && peran === 'Tank'){
-	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat melindungi temanmu di barisan depan"');
-} else if (nama === 'Joko' && peran === 'Support') {
-	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menyembuhkan temanmu yang terluka"');
-} else if (nama === 'Joko' && peran === 'Archer') {
-	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menyembuhkan temanmu yang terluka"');
-} 
-
-// Memilih nama Ali
-else if (nama === 'Ali' && peran === 'Tank'){
-	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat melindungi temanmu di barisan depan"');
-} else if (nama === 'Ali' && peran === 'Support') {
-	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menyembuhkan temanmu yang terluka"');
-} else if (nama === 'Ali' && peran === 'Archer') {
-	console.log('"Selamat datang di Dunia Proxytia, ' + nama + '"' + '\n"Halo, ' + peran + " " + nama + ', kamu dapat menyembuhkan temanmu yang terluka"');
 } 
 
 // Nama kosong
